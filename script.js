9b1305dbe8de105743fc08911abcb6b77e2662b1
AWNFKJMLsbnsjmkl <79947384+EyelessHairball@users.noreply.github.com>
2024-05-20 02:02:29 -0700

    added javascript, mobile redirector

---
+++ script.js
@@ -0,0 +1,28 @@
+const isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
+if( isMobile.any()(confirm('Mobile Browser detected. Would you like to move to the mobile page? [its just better formatting for mobile]'))) {
+    // Save it!
+    console.log('test! hooray!');
+  } else {
+    // Do nothing!
+    console.log('no mobile page!');
+  }
